accc0be3925c60698c1863a242ebabd50fa73f22
Diego S. Seabra <dss-diego@hotmail.com>
2025-11-13 01:54:12 -0300

    CHANGE: Power Up chooser navigation
    With wrapping

--- Source/UI/Modals/PowerUpChooser.cs
+++ Source/UI/Modals/PowerUpChooser.cs
@@ -9,34 +9,68 @@ public class PowerUpChooser : UI
     private Vector2 basePosition = new(200, 200);
 
     private int offsetX = 200;
+    private int selectedCardIdx = 0;
 
     public PowerUpChooser(Controls controls)
     {
         Controls = controls;
         
-        powerUpCards.Add(new Card(true));
         powerUpCards.Add(new Card());
         powerUpCards.Add(new Card());
+        powerUpCards.Add(new Card());
+        
+        UpdateSelection();
+    }
+
+    private void UpdateSelection()
+    {
+        foreach (var card in powerUpCards)
+        {
+            card.SetState(Card.State.Normal);
+        }
+        
+        powerUpCards[selectedCardIdx].SetState(Card.State.Selected);
     }
 
     public override void Update()
     {
-        // navigate left
-        if (Controls.MenuNavLeft.ConsumePress())
-        {
-            
-        }
-
         // navigate right
         if (Controls.MenuNavRight.ConsumePress())
         {
+            if (selectedCardIdx < powerUpCards.Count - 1)
+            {
+                selectedCardIdx++;
+            }
+            else
+            {
+                selectedCardIdx = 0;
+            }
+
+            UpdateSelection();
+        }
+
+        // navigate left
+        if (Controls.MenuNavLeft.ConsumePress())
+        {
+            if (selectedCardIdx > 0)
+            {
+                selectedCardIdx--;
+            }
+            else
+            {
+                selectedCardIdx = powerUpCards.Count - 1;
+            }
             
+            UpdateSelection();
         }
 
         // confirm/press
         if (Controls.MenuConfirm.ConsumePress())
         {
             // TODO: Apply the buff in the game
+            
+            // make the game "run" again
+            Game.ChangeState(Game.GameState.Running);
         }
     }
 
@@ -59,7 +93,6 @@ public class PowerUpChooser : UI
 
                 offset += offsetX;
             }
-            
         }
     }
 }
